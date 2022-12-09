a3481922e8f92dcb8fd2184febc3be3f335ff2fe
Sir-Blow <emmanuelakej@gmail.com>
2022-12-08 21:06:13 -0800

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,17 +3,17 @@
 
 
 /**
- *
  *  * main - Entry point
  *
  *   *
  *
  *    * Return: Always 0 (success)
  *
- *     */
+ *    
+ *
+ */
 
 int main(void)
-
 {
 
 		printf("with proper grammar, but the outcome is a piece of art,\n");
