70f70e9207eec951c5f63340c9ca9858102b9587
Sir-Blow <emmanuelakej@gmail.com>
2022-12-13 10:41:10 -0800

    0 Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point of print putchar
+ * Return:always 0 (SUCCESS)
+ *
+ */
+
+int main(void)
+{
+	_putchar(' ');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+the Puchar directory
