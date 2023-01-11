bbb54649f50fdda390ac169563fb7a1fa8b20320
Sir-Blow <emmanuelakej@gmail.com>
2023-01-11 03:50:14 -0800

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,13 +18,13 @@ char *_strdup(char *str)
 	i = 0;
 
 	while (str[i] != '\0')
-		i++;
+	i++;
 	aaa = malloc(sizeof(char) * (i + 1));
 
 	if (aaa == NULL)
 	return (NULL);
 
 	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
+	aaa[r] = str[r];
 	return (aaa);
 }
