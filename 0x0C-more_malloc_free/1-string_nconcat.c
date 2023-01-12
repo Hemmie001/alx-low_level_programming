99b28de57d49c75f10b53a10b118d8e6e526798d
Sir-Blow <emmanuelakej@gmail.com>
2023-01-12 08:45:21 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,9 +19,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2 && s2[len2])
 	len2++;
 	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
+	s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	s = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!s)
 	return (NULL);
 	while (i < len1)
@@ -30,9 +30,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
+	s[i++] = s2[j++];
 	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-		s[i] = '\0';
+	s[i++] = s2[j++];
+	s[i] = '\0';
 	return (s);
 }
