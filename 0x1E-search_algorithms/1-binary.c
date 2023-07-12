f83d0cba77d03f0e688433205d612c16414bfc5d
Sir-Blow <emmanuelakej@gmail.com>
2023-07-12 14:24:08 -0700

    1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,57 +1,39 @@
 #include "search_algos.h"
 
-void print_array(int *array, int left, int right);
 /**
-* binary_search - searches for a value in an array of
-* integers using the binary search algorithm
-* @array: is a pointer to the first element of the array to search in
-* @size: the nmber of elements in array
-* @value: is the value to search for
-* Return: If value is not present in array or if array
-* is NULL, your function must return -1
-*/
+ * binary_search - searches for a value in a sorted array of integers,
+ * using the Binary search algorithm.
+ * @array: pointer to the first element of the array to search in.
+ * @size: number of elements in array.
+ * @value: value to search for.
+ * Return: first index where value is located,
+ * or -1 if value is not present or array is NULL.
+ **/
 
 int binary_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
+	size_t i, left, right;
+
+	if (!array)
 		return (-1);
-	return (binary_search_rec(array, 0, (int)size - 1, value));
-}
 
-/**
-* binary_search_rec - recursion for binary search algorithm
-* @array: is a pointer to the first element of the array to search in
-* @left: the less index of the array
-* @right: the highest index of the array
-* @value: is the value to search for
-* Return: If value is not present in array return -1
-*/
-int binary_search_rec(int *array, int left, int right, int value)
-{
-	int mid = left + (right - left) / 2;
+	left = 0;
+	right = size - 1;
+	while (right >= left)
+	{
+		/* print the array */
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-	print_array(array, left, right);
-	if (array[mid] == value)
-		return (mid);
-	if (left >= right)
-		return (-1);
-	if (array[mid] < value)
-		return (binary_search_rec(array, mid + 1, right, value));
-	return (binary_search_rec(array, left, mid - 1, value));
-}
-
-/**
-* print_array - print the array
-* @array: is a pointer to the first element of the array to search in
-* @left: the less index of the array
-* @right: the highest index of the array
-*/
-void print_array(int *array, int left, int right)
-{
-	int i = left;
-
-	printf("Searching in array: ");
-	for (; i < right; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
+		i = left + (right - left) / 2;
+		if (array[i] < value)
+			left = i + 1;
+		else if (array[i] > value)
+			right = i - 1;
+		else
+			return (i);
+	}
+	return (-1);
 }
